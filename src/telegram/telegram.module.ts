c5595a8e640cefe8906177217a94ab0874912802
ysenpi <168177815+ysenpi@users.noreply.github.com>
2025-05-22 02:22:20 +0300

    Update telegram.module.ts

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common'
-import { NestjsGrammyModule } from '@grammyjs/nestjs'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { InjectBot, NestjsGrammyModule } from '@grammyjs/nestjs'
+import { Bot, Context, webhookCallback } from 'grammy'
 import { TelegramUpdate } from './telegram.update'
+import { TelegramService } from './telegram.service'
 import { telegramConfig } from 'src/config/telegram.config'
 
 @Module({
   imports: [NestjsGrammyModule.forRootAsync(telegramConfig)],
-  providers: [TelegramUpdate]
+  providers: [TelegramUpdate, TelegramService]
 })
-export class TelegramModule {}
+export class TelegramModule implements NestModule {
+  constructor(
+    @InjectBot() private readonly bot: Bot<Context>,
+    private readonly config: ConfigService
+  ) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    const webhookPath = this.config.getOrThrow<string>('BOT_WEBHOOK_PATH')
+    consumer.apply(webhookCallback(this.bot, 'express')).forRoutes(`/${webhookPath}`)
+    console.log('[bot] webhook mode')
+  }
+}
